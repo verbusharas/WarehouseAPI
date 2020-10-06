610f2b84d5d908dcb202d8a71d64de25e24123c3
Sarunas Verbus <sarunas@verbus.lt>
2020-10-06 21:36:26 +0300

    ProductRepository annotated as @Repository following good practice.

--- src/main/java/lt/verbus/vismawarehouse01/repository/ProductRepository.java
+++ src/main/java/lt/verbus/vismawarehouse01/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package lt.verbus.vismawarehouse01.repository;
 
 import lt.verbus.vismawarehouse01.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
